aed51f26c356e46ed31a60b97d4ee8aefa616405
Guillaume <gumgl@users.noreply.github.com>
2015-03-21 16:46:53 -0400

    /: Add previous projects

---
+++ 31/31.hs
@@ -0,0 +1,7 @@
+engcoins = [200, 100, 50, 20, 10, 5, 2, 1]
+
+count _ 0 = 1
+count [] _ = 0
+count (coin:coins) money = sum $ map (\i -> count coins (money - i*coin)) [0 .. (money `div` coin)]
+
+answer = count engcoins 200
