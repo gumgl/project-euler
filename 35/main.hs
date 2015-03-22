746c087acad007b3e700d87e956eb1ae0b5fb92f
Guillaume <gumgl@users.noreply.github.com>
2015-03-21 20:52:10 -0400

    /35: Gives the correct answer

--- 35/main.hs
+++ 35/main.hs
@@ -1,13 +1,21 @@
 import Data.List
 import Data.Digits
 import Data.Numbers.Primes
-import qualified Data.Map as Map 
-maxim = 999999
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
--- get content of list below max
-below :: Integral a => a -> [a] -> [a]
 below _ [] = []
-below max (x:xs) = if (x <= max) then [x] ++ below(max xs) else []
+below max (x:xs) = if (x < max) then [x] ++ (below max xs) else (below max xs)
+
+-- get content of list below max and stop (when list is growing)
+belowStop :: Integral a => a -> [a] -> [a]
+belowStop _ [] = []
+belowStop max (x:xs) = if (x < max) then [x] ++ (belowStop max xs) else []
+
+maxim = 1000000
+
+theprimes = Set.fromList (belowStop maxim primes)
+-- TODO: optimize so that we don't look at numbers containing an even digit with filter&map
 
 -- rotate a list
 rotate :: Int -> [a] -> [a]
@@ -18,9 +26,20 @@ rotate n xs = zipWith const (drop n (cycle xs)) xs
 rotations :: [a] -> [[a]]
 rotations list = map (\i -> rotate i list) [0 .. length(list)-1]
 
+-- rotations of an integer
 nrotations x = map (\i -> unDigits 10 i) (rotations (digits 10 x))
 
 -- generate all rotations, then each back to numbers, then take the minimum
 hash x = minimum (nrotations x)
 
---map (\i -> i) (
+-- If there is a rotation smaller than x not in the set, it should've been added before and is therefore not a circular prime
+--addNumber :: Integer a => a -> Set.Set a -> Set.Set a
+--addNumber [] s = s
+--addNumber (x:xs) s = if (null (filter (\i -> (not (Set.member i s))) (below x (nrotations x)))) then (addNumber xs (Set.insert x s)) else (addNumber xs s)
+
+isPrimeEfficient x = Set.member x theprimes
+
+-- generate all circular primes in a set
+isCircular x = all isPrimeEfficient (nrotations x)
+
+circulars = filter isCircular [2..maxim]
