fc56d5dd99607080e29753202203e5c170501569
Guillaume <gumgl@users.noreply.github.com>
2015-03-21 21:25:22 -0400

    /35: Optimize and make it compilable

--- 35/main.hs
+++ 35/main.hs
@@ -12,11 +12,6 @@ belowStop :: Integral a => a -> [a] -> [a]
 belowStop _ [] = []
 belowStop max (x:xs) = if (x < max) then [x] ++ (belowStop max xs) else []
 
-maxim = 1000000
-
-theprimes = Set.fromList (belowStop maxim primes)
--- TODO: optimize so that we don't look at numbers containing an even digit with filter&map
-
 -- rotate a list
 rotate :: Int -> [a] -> [a]
 rotate _ [] = []
@@ -26,20 +21,24 @@ rotate n xs = zipWith const (drop n (cycle xs)) xs
 rotations :: [a] -> [[a]]
 rotations list = map (\i -> rotate i list) [0 .. length(list)-1]
 
+isEven x = (x `mod` 2) == 0
+isOdd x = not $ isEven x
+
+allOdd x = all isOdd (digits 10 x)
+
+primesList = belowStop 1000000 (2:(filter allOdd primes))
+primesSet = Set.fromList primesList
+
 -- rotations of an integer
 nrotations x = map (\i -> unDigits 10 i) (rotations (digits 10 x))
 
--- generate all rotations, then each back to numbers, then take the minimum
-hash x = minimum (nrotations x)
-
--- If there is a rotation smaller than x not in the set, it should've been added before and is therefore not a circular prime
---addNumber :: Integer a => a -> Set.Set a -> Set.Set a
---addNumber [] s = s
---addNumber (x:xs) s = if (null (filter (\i -> (not (Set.member i s))) (below x (nrotations x)))) then (addNumber xs (Set.insert x s)) else (addNumber xs s)
-
-isPrimeEfficient x = Set.member x theprimes
+isPrimeEfficient x = Set.member x primesSet
 
 -- generate all circular primes in a set
 isCircular x = all isPrimeEfficient (nrotations x)
 
-circulars = filter isCircular [2..maxim]
+circulars = filter isCircular primesList
+
+answer = length circulars
+
+main = print answer
