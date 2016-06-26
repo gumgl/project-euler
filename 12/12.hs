93363c8e95b7453b64c51d1c42f2029a998b6b98
Guillaume <gumgl@users.noreply.github.com>
2016-06-26 13:14:50 -0400

    Complete problem 12

---
+++ 12/12.hs
@@ -0,0 +1,7 @@
+import qualified Math.NumberTheory.Primes.Factorisation as F
+
+triangles :: [Integer]
+triangles = triangle 0 0
+    where triangle sum index = (sum+index+1) : triangle (sum+index+1) (index+1)
+	
+answer = head $ filter (\i -> (length $ F.divisors i) > 500) triangles
