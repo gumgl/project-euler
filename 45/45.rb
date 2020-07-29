ac04c2469f7b4d81a19b9d8444b51e8edc2e91e8
gumgl <gumgl@users.noreply.github.com>
2020-07-29 15:24:13 -0700

    Solve problem 45

---
+++ 45/45.rb
@@ -0,0 +1,21 @@
+# Using the quadratic equation, we can derive closed-form
+# tests for these properties.
+
+# Note: Triangular test not needed but included for completeness
+def is_triangular?(i)
+	return (Math.sqrt(8 * i + 1) % 1).zero?
+end
+
+def is_pentagonal?(i)
+	return (((Math.sqrt(24 * i + 1) + 1) / 6) % 1).zero?
+end
+
+def is_hexagonal?(i)
+	return (((Math.sqrt(8 * i + 1) + 1) / 4) % 1).zero?
+end
+
+triangle_l = (1..).lazy.map{|n| n*(n+1)/2}
+
+solutions_l = triangle_l.select{|n| is_pentagonal?(n) and is_hexagonal?(n)}
+
+puts solutions_l.find{|n| n > 40755}
