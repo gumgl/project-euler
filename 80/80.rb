374c8e66c26dccfca783d95ddbc30e42d90cf3b1
gumgl <gumgl@users.noreply.github.com>
2020-07-30 04:21:07 -0700

    Generalize problem 80 a tiny bit

--- 80/80.rb
+++ 80/80.rb
@@ -1,9 +1,10 @@
 require 'bigdecimal'
 
-n = 100
+numbers = 100
+precision = 1000
 
-puts (1..n).select{|i| Math.sqrt(i).floor()**2 != i} # Select irrationals
-	.map{|i| BigDecimal(i).sqrt(n+1)} # Get roots
-	.map{|r| r.to_s("F")[0,n+1].tr('.','').chars.map(&:to_i)} # Get digits
+puts (1..numbers).select{|i| Math.sqrt(i).floor()**2 != i} # Select irrationals
+	.map{|i| BigDecimal(i).sqrt(precision+1)} # Get roots
+	.map{|r| r.to_s("F")[0,precision+1].tr('.','').chars.map(&:to_i)} # Get digits
 	.map{|d| d.sum} # Get sums of digits
 	.sum
