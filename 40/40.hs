a5e0411651a1c4f5de2e648a730b580ebbbac7ae
gumgl <gumgl@users.noreply.github.com>
2020-11-18 15:24:50 -0800

    Solve problem 40

---
+++ 40/40.hs
@@ -0,0 +1,14 @@
+-- There might be a clever way to work out the digits on paper
+-- by counting the number sizes, but this is faster.
+
+import Data.Char (ord)
+
+digitToInt d = (ord d) - (ord '0')
+
+frac = concat $ map show [0..]
+
+digits = map (\i -> digitToInt $ frac !! (10^i)) [0..6]
+
+answer = foldr (*) 1 digits
+
+main = print answer
