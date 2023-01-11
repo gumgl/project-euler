d37294d7dd24e2f57332ed7203785dd021c51063
Guillaume <gumgl@users.noreply.github.com>
2023-01-10 17:30:52 -0800

    Solve problem 53

---
+++ 53/53.hs
@@ -0,0 +1,5 @@
+factorial n = product [1..n]
+
+choose n r = (factorial n) `div` ((factorial r) * factorial (n-r))
+
+answer = length $ filter (> 10^6) $ map (uncurry choose) [(n,r) | n <- [1..100], r <- [1..n]]
