fcda2c44ced00a737782f6a2dec60246b3b827f1
Guillaume <gumgl@users.noreply.github.com>
2023-01-12 23:38:15 -0800

    Clean up problem 71 a bit

--- 71/71.hs
+++ 71/71.hs
@@ -2,16 +2,15 @@ import Data.List
 import Data.Function ( on )
 import Data.Ratio ( (%), numerator, denominator, Ratio )
 
-target = 3 % 7
-
--- Select one fraction for every d where n/d is as close to target but below
-fractions :: Integer -> [Ratio Integer]
-fractions x = [n % d | d <- [2..x],
-               let n = ((numerator target) * d) `div` (denominator target), -- int div rounding down
-               n % d < target] -- filter fractions that reduce to target
+-- Select one fraction for every d where n/d is as close to target but below it
+fractions :: Ratio Integer -> Integer-> [Ratio Integer]
+fractions target maxD =
+  [n % d | d <- [2..maxD],
+   let n = ((numerator target) * d) `div` (denominator target), -- int div rounding down
+   n % d < target] -- filter fractions that reduce to target
 
 -- Select the one with the smallest difference from target
-answer :: Integer -> Ratio Integer
-answer = minimumBy (compare `on` (target -)) . fractions
+answer :: Ratio Integer -> Integer -> Ratio Integer
+answer target = minimumBy (compare `on` (target -)) . (fractions target)
 
-main = print $ answer $ 10^6
+main = print $ answer (3 % 7) (10^6) 
