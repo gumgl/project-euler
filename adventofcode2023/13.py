3cfe466a7fa94f9f86892a90b6a890aa9189d372
Guillaume <gumgl@users.noreply.github.com>
2023-12-14 23:30:00 +0100

    Tidy up AOC2023 #13 a bit

--- adventofcode2023/13.py
+++ adventofcode2023/13.py
@@ -3,7 +3,7 @@ from Helpers import keys_by_value
 
 input_file = open('13_input.txt', 'r')
 
-input_grids = [[[*line.strip()] for line in single_input.splitlines()] for single_input in input_file.read().split('\n\n')]
+input_grids = [[[*line.strip()] for line in input_grid.splitlines()] for input_grid in input_file.read().split('\n\n')]
 
 def find_symmetries(haystack):
   """Returns k indices [i_k] where there are axes of symmetry
@@ -20,15 +20,10 @@ def find_vertical_symmetry(grid, look_for_smudge):
   return keys[0] if (keys := list(keys_by_value(counts, target_count))) else 0
 
 # General strategy: Grid-level vertical symmetries happen when row-level symmetries align (set intersection).
-# For smudges, we simply look for symmetries across all rows except one.
-# For horizontal symmetry, we simply transpose the grid (flip along diagonal / swap rows and columns)
+# For smudges, we look for symmetries aligned across all rows except one.
+# For horizontal symmetry, we simply transpose the grid (flip along diagonal / swap rows and columns).
 def part(num):
-  total = 0
-  for grid in input_grids:
-    v = find_vertical_symmetry(grid, num == 2)
-    h = find_vertical_symmetry(list(zip(*grid)), num == 2)
-    total += v or 100 * h
-  return total
+  return sum(find_vertical_symmetry(grid, num == 2) or 100 * find_vertical_symmetry(list(zip(*grid)), num == 2) for grid in input_grids)
 
 print(part(1))
 print(part(2))
